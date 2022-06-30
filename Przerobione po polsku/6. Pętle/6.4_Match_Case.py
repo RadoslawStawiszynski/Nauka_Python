3ea109eb32ab99b5c8521330c96798fac165f471
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-06-30 08:10:45 +0200

    Match - case =gotowy

--- Przerobione po polsku/6. Pętle/6.4_Match_Case.py
+++ Przerobione po polsku/6. Pętle/6.4_Match_Case.py
@@ -32,18 +32,94 @@ print("Przykład nr 2")
 
 
 def calc(n: int):
-    return (n ** 2)
+    return n ** 2
 
 
-def calc(n: float):
-    return (n ** 3)
+def calc2(n: float):
+    return n ** 3
 
 
-n = 9.5
+n = 2
 is_int = isinstance(n, int)
 
 match is_int:
     case True:
         print("Square is:", calc(n))
     case False:
-        print("Cube is:", calc(n))
+        print("Cube is:", calc2(n))
+
+print()
+print("Przykład nr 3")
+
+quit_flag = 4
+
+match quit_flag:
+    case True:
+        print("Quitting")
+        exit()
+    case False:
+        print("System is on")
+    case _:
+        print("Boolean Value was not passed")
+
+
+print()
+print("Przykład nr 4")
+
+sample = True
+
+match sample:
+    case (True | False):
+        print("It is a boolean value")
+    case _:
+        print("Not a boolean value")
+
+
+print()
+print("Przykład nr 5")
+
+list1 = ['a', 'b', 'c', 'd']
+
+match list1:
+    case ['e', 'f']:
+        print("e,f present")
+    case ['a', 'b', 'c', 'd']:
+        print("a,b,c,d present")
+
+
+
+print()
+print("Przykład nr 6")
+
+
+class switch:
+    on = 1
+    off = 0
+
+
+status = 0
+
+match status:
+    case switch.on:
+        print('Switch is on')
+    case switch.off:
+        print('Switch is off')
+
+
+
+print()
+print("Przykład nr 7")
+
+n = 0
+match n:
+    case n if n < 0:
+        print("Number is negative")
+    case n if n == 0:
+        print("Number is zero")
+    case n if n > 0:
+        print("Number is positive")
+
+
+print()
+print("Przykład nr 8")
+
