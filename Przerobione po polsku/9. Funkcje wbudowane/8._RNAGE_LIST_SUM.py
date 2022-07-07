8d09b23176d192e4fa7fb3ae3d49f2b1c0ec2b7b
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-07-07 21:34:25 +0200

    funkcja wbudowana 8

---
+++ Przerobione po polsku/9. Funkcje wbudowane/8._RNAGE_LIST_SUM.py
@@ -0,0 +1,12 @@
+# funkcja RANGE służy do określenia ilości powtórzeń w danym przedziale
+
+for x in range(0, 6):
+    print(x)
+
+# LUB
+
+print(list(range(0, 6)))
+print(list(range(0, 21, 2)))
+
+print(sum(range(0, 50, 5)))
+
