25298c4058ccf6cfd25c99887fd3e4c3596065f4
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-07-07 18:23:01 +0200

    funkcja wbudowana 6

---
+++ Przerobione po polsku/9. Funkcje wbudowane/6._FLOAT_INT.py
@@ -0,0 +1,15 @@
+# funkcja FLOAT służy do przekształcania liczby całkowitej na liczbę dziesiętną (zmiennoprzecinkową)
+# funkcja INT służy do przekształacania liczby dziesietnej na liczbę całkowitą
+
+num1 = 10
+num2 = 10.01
+
+print(float(num1))
+print(float(num2))
+
+print(int(num1))
+print(int(num2))
+print(dir(int))
+
+
+
