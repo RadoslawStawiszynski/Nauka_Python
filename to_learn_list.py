eee8741eed81b997a714eaa517925ef3bd1b2db6
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-07-07 15:43:19 +0200

    Lista czego się jeszcze nauczyć.

---
+++ to_learn_list.py
@@ -0,0 +1,12 @@
+# Flask – https://flask.palletsprojects.com (Quickstart)
+#       SQLAlchemy
+# Django – https://www.djangoproject.com/start/ (Getting started)
+#       Django ORM
+#       MongoDB
+# SciPy
+# NumPy
+# Matplotlib
+# unittest
+# pytest
+# Docker
+#  rozwiązaniami chmurowymi AWS
