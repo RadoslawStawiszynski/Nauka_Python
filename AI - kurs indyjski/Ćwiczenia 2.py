611673247daf14da8719c2e38e0c28a1ca840b1f
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-07-26 17:52:07 +0200

    .... praca nad funkcja sortowania

--- AI - kurs indyjski/Ćwiczenia 2.py
+++ AI - kurs indyjski/Ćwiczenia 2.py
@@ -1,4 +1,3 @@
-
 # Sortowanie liczb
 print('ZADANIE - Sortowanie liczb')
 # pierwsza liczba jest większa od drugiej
@@ -6,8 +5,37 @@ print('ZADANIE - Sortowanie liczb')
 L1 = [2, 5, 7, -3, 2, 4, -9, 0, 1, -8]
 min_l = L1[0]
 L2 = []
-dl_L = len(L1)
 
+
+def min_number(self):
+    min_l = self[0]
+    i = 0
+    while i < len(self):
+        if self[i] < min_l:
+            min_l = self[i]
+        else:
+            i += 1
+            pass
+    print('Liczba minimalna wynosi:', min_l)
+
+
+def sort_number(self):
+    min_l = self[0]
+    i = 0
+
+    while i < len(self):
+        if self[i] < min_l:
+            min_l = self[i]
+        else:
+            i += 1
+            pass
+    print('Liczba minimalna wynosi:', min_l)
+
+
+print(min_number(L1))
+print(sort_number(L1))
+
+'''
 for i in L1:
     if i == dl_L - 1:
         i = 0
@@ -22,5 +50,4 @@ for i in L1:
         print("Koniec")
 
     # print(L2)
-
-
+'''
