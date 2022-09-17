66c3d8077c45f0892a0004bdc3088f19aa13e098
Radosław Stawiszyński <radoslaw.stawiszynski@gmail.com>
2022-09-17 21:30:14 +0200

    Mod photo - w budowie

--- Projekty do CV/Projekt nr 2 - Modifikacje obrazów_do poprawy/mod_photo.py
+++ Projekty do CV/Projekt nr 2 - Modifikacje obrazów_do poprawy/mod_photo.py
@@ -1,1 +1,9 @@
-heloo
+# python grayscale.py --input color --output blackwhite
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="Zamiana jpgów na czarno-bia³e")
+parser.add_argumnet('--input')
+args = parser.parse_args()
+print(args)
+
