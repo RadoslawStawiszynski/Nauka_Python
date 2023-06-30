ca011b2f01079bf5d93c398c78247688c05ebd4f
RadoslawStawiszynski <radoslaw.stawiszynski@gmail.com>
2023-06-30 19:57:40 +0200

    Praca nad logiką gry.

--- Projekty do CV/Projekt nr 1 - Szukamy klucza/key_v1.py
+++ Projekty do CV/Projekt nr 1 - Szukamy klucza/key_v1.py
@@ -1,82 +1,110 @@
 from random import randint
 import math
 
-# Game settings
-game_width = 3
-game_height = 3
+# The GAME class
+class Game:
+    # Generate random positions for the key and door
+    # Game settings
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.level = 0
+        self.steps = 0
+        self.steps_max = 15
+        self.player_found_key = False
+        self.player_x = 0
+        self.player_y = 0
+        self.key_x = randint(0, self.width)
+        self.key_y = randint(0, self.height)
+        self.door_x = randint(0, self.width)
+        self.door_y = randint(0, self.height)
+        self.distance_before_move = math.sqrt((self.key_x - self.player_x) ** 2 + (self.key_y - self.player_y) ** 2)
+        self.distance_after_move = 0
+    # Defining the goals of the game/level
+    def start(self):
+        #print("Key: x =", self.key_x, " y =", self.key_y)
+        #print("Door: x =", self.door_x,  " y =", self.door_y)
+        print()
 
-# Generate random positions for the key and door
-key_x = randint(0, game_width)
-key_y = randint(0, game_height)
-
-door_x = randint(0, game_width)
-door_y = randint(0, game_height)
-
-player_x = 0
-player_y = 0
-
-level = 0
-
-steps = 0
-distance_before_move = math.sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
-
-player_found_key = False
-
-print("key: ", key_x, key_y)
-print("door: ", door_x, door_y)
-
-while not player_found_key:
-    steps += 1
-    print()
-    print('''You can move in the directions [W/A/S/D]:
-Press [Q] to quit the game.''')
-    move = input("Where do you want to go?")
-    match move.lower():
-        case "w":
-            player_y += 1
-            if player_y > game_height:
-                print("You have reached the top boundary of the board.")
-                player_y = game_height
-        case "a":
-            player_x -= 1
-            if player_x < 0:
-                print("You have reached the left boundary of the board.")
-                player_x = 0
-        case "s":
-            player_y -= 1
-            if player_y < 0:
-                print("You have reached the bottom boundary of the board.")
-                player_y = 0
-
-        case "d":
-            player_x += 1
-            if player_x > game_width:
-                print("You have reached the right boundary of the board.")
-                player_x = game_width
-        case "q":
-            print('Game over')
+        while self.steps < self.steps_max:
+            print('''You can move in the directions [W/A/S/D]: \n Press [Q] to quit the game.''')
+            move = input(f"You have left {self.steps_max-self.steps} steps. Where do you want to go? ")
+            self.steps += 1
+            self.move_player(move.lower())
+        while self.steps == self.steps_max:
+            print("GAME OVER")
             quit()
-        case _:
-            print("I don't know where you want to go...")
-            continue
 
-    if player_x == key_x and player_y == key_y:
-        print("Congratulations! You found the treasure! Now you can go open the door. The door is at ", door_x, "  ", door_y)
-        text_1 = "You took %s steps."
-        print(text_1 % steps)
+    # Defining player moves
+    def move_player(self, move):
+        # W A S D moves
+        match move.lower():
+            case "w":
+                self.player_y += 1
+                if self.player_y > self.height:
+                    print("You have reached the top boundary of the board.")
+                    self.player_y = self.height
+                    self.steps -= 1
+            case "a":
+                self.player_x -= 1
+                if self.player_x < 0:
+                    print("You have reached the left boundary of the board.")
+                    self.player_x = 0
+                    self.steps -= 1
+            case "s":
+                self.player_y -= 1
+                if self.player_y < 0:
+                    print("You have reached the bottom boundary of the board.")
+                    self.player_y = 0
+                    self.steps -= 1
+            case "d":
+                self.player_x += 1
+                if self.player_x > self.width:
+                    print("You have reached the right boundary of the board.")
+                    self.player_x = self.width
+                    self.steps -= 1
+            case "q":
+                print('Game over')
+                quit()
+            case _:
+                print("I don't know where you want to go...")
+                self.steps -= 1
 
-    distance_after_move = math.sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
+        # Middle ending game - Get KEY
+        if self.player_x == self.key_x and self.player_y == self.key_y:
+            print("Congratulations! You found the treasure! Now you can go open the door.")
+            #print("The door is at x = ", self.door_x, " y = ", self.door_y)
+            print(f'You took {self.steps} steps.')
+            self.player_found_key = True
 
-    if distance_before_move > distance_after_move:
-        print("You are closer to the key.")
-    else:
-        print("You are farther from the key.")
+        # Logic of the moves
+        self.distance_after_move = math.sqrt((self.key_x - self.player_x) ** 2 + (self.key_y - self.player_y) ** 2)
 
-    distance_before_move = distance_after_move
+        # Hints for the full game
+        while True:
+            if self.player_found_key == False:
+                goal = "key"
+            else:
+                goal = "door"
 
-    if player_x == door_x and player_y == door_y:
-        print(f'Congratulations! You finished Level {level}.')
-        level += 1
-        quit()
+            if self.distance_before_move > self.distance_after_move:
+                print(f"You are closer to the {goal}.")
+            else:
+                print(f"You are farther from the {goal}.")
+            break
 
-    print(player_x, player_y)
+        self.distance_before_move = self.distance_after_move
+
+        # Last ending game/level
+        if self.player_x == self.door_x and self.player_y == self.door_y and self.player_found_key == True:
+            print("x =", self.player_x, " y =", self.player_y)
+            print(f'Congratulations! You finished Level {self.level}.')
+            print(f'You took {self.steps} steps.')
+            self.level += 1
+            quit()
+
+        print("x =", self.player_x, " y =", self.player_y)
+
+# Create a game instance and start the game
+game = Game(3, 3)
+game.start()
